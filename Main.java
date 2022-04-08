28f4c3d2be4dd985c3dd932fd5e02a9ba86a0879
Плотко Александр <plotko1985@gmail.com>
2022-04-08 22:28:40 +0700

    calculator

---
+++ Main.java
@@ -0,0 +1,15 @@
+package calkulyatorNetologe;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
+        int d = calc.multiply.apply(c, 5);
+
+        calc.println.accept(d);
+
+    }
+}
